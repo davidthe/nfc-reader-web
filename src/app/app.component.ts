6cfe97b2748b0353d74c30958f3059eb823a6bf3
David Mizrchi <david@aka.co.il>
2022-12-16 00:10:45 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { AudioService } from './audio-service.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  constructor(public translate: TranslateService,private audioService: AudioService ) {
+    translate.addLangs(['en', 'ar', 'he']);
+    translate.setDefaultLang('he');
+    this.initAndstartScanNFC();
+  }
+  
+  switchLang(lang: string) {
+    this.translate.use(lang);
+  }
+
+  initAndstartScanNFC(){
+    try {
+      const ndef = new NDEFReader();
+      ndef.scan().then(() => {
+        ndef.onreading = event => {
+          console.log(event.serialNumber);
+          console.log(event)
+          this.audioService.getFiles().subscribe(files=>{
+            const file = files[Math.floor(Math.random() * 3)];
+            console.log(file.url);
+
+            this.audioService.playStream(file.url) .subscribe(events => {
+              // listening for fun here
+            });
+            this.audioService.pause();
+            this.audioService.play();
+          });
+          
+          // this.musicPlayerService.addTrack()
+        };
+      });
+      console.log("> Scan started");
+    } catch (error) {
+      console.log("Argh! " + error);
+      alert("your device dosent support nfc")
+    }
+  }
+}

60bda8126ece396ed8973c1b6d33d1b20730f640
David Mizrchi <david@aka.co.il>
2022-12-16 02:15:11 +0200

    changed to react

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { useNfc } from 'use-nfc-hook';
+
+const App = () => {
+  const { isNDEFAvailable, permission, read, abortReadCtrl, write } = useNfc()
+  
+  // decode an NFC tag containing a single record
+  const handleRead = async () => {
+    try {
+      
+      const response = await read()
+
+      const record = response.message.records[0]
+
+      const decoder = new TextDecoder('utf-8');
+
+      const decodedContent = decoder.decode(record.data)
+
+      console.log("DECODED CONTENT:", decodedContent);
+
+    } catch (error) {
+      console.log("ERROR ", error);
+    }
+  }
+  
+  return (
+    <>
+      {isNDEFAvailable !== undefined && !isNDEFAvailable && (
+        <div>Looks like NDEF is not available</div>
+      )}
+      {isNDEFAvailable && <button
+        onClick={handleRead}
+        // disabled={permission as PermissionState === 'denied'}
+      >
+        Start Scan
+      </button>}
+    
+    
+    {permission}
+    </>
+  );
+}
+export default App;
